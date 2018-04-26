8b57fe167d430b985c330c636a2f6ec2add3b5d5
chang93 <chang.sala21@gmail.com>
2018-04-26 15:41:29 -0500

    updated piece

--- src/checkers/Piece.java
+++ src/checkers/Piece.java
@@ -2,6 +2,7 @@ package checkers;
 
 import java.util.Scanner;
 
+
 import checkers.Coordinates;
 
 public class Piece{
@@ -31,115 +32,172 @@ public class Piece{
 		this.kinged = kinged;
 	}
 	
-	private void whereTo() {
+	//====================================================
 		
-		System.out.println("Enter the X coordinate of the piece you want to move:");
-		Scanner keyX = new Scanner(System.in);		
-		int locX = keyX.nextInt();
+		private static int blackChipCounter = 0;
+		private static int redChipCounter = 0;
 		
-		System.out.println("Enter the Y coordinate of the piece you want to move:");
-		Scanner keyY = new Scanner(System.in);		
-		int locY = keyY.nextInt();
-		Boolean color = true;
-		Boolean king = false;
+		public static void whereTo() {
+		
+			Piece[][] myBoard = Driver.getBoard();
+			System.out.println("Enter the X coordinate of the piece you want to move:");
+			Scanner keyX = new Scanner(System.in);		
+			int locX = keyX.nextInt();
+			
+			System.out.println("Enter the Y coordinate of the piece you want to move:");
+			Scanner keyY = new Scanner(System.in);		
+			int locY = keyY.nextInt();
+			
+			Boolean color = true;
+			Boolean king = false;
 
-		
-		//black King
-		if (color == true && king == true) {	
-			System.out.println("Black King: moving left, right, back left, or back right?");
-			Scanner kingBlackLR = new Scanner(System.in);		
-			String kingBlackMove = kingBlackLR.nextLine();		// prompt user for left, right, back left, back right - STRING or CHAR??
 			
-			if(kingBlackMove.equals("right")) {			// moves right			
-				int tempX = locX - 1;
-				int tempY = locY + 1;
-				System.out.println("I am : King, Black, moved forward right");
-			}
-			else if(kingBlackMove.equals("left")) {		// moves left
-				int tempX = locX + 1;
-				int tempY = locY + 1;
-				System.out.println("I am : King, Black, moved forward left");
-			}
-			else if(kingBlackMove.equals("back right")) {	// moves back right
-				int tempX = locX + 1;
-				int tempY = locY - 1;
-				System.out.println("I am : King, Black, moved backwards right");
-			}
-			else if(kingBlackMove.equals("back left")) {	// moves back left
-				int tempX = locX - 1;
-				int tempY = locY - 1;
-				System.out.println("I am : King, Black, moved backwards left");
-			}
-		}
-		//---------------------------------------------------------------------
-		//red King	
-			else if(color == false && king == true) {	
-				System.out.println("Red King: moving left, right, back left, or back right?");
-				Scanner kingRedLR = new Scanner(System.in);		
-				String kingRedMove = kingRedLR.nextLine();		// prompt user for left, right, back left, back right - STRING or CHAR??
+			//black King
+			if (color == true && king == true) {	
+				System.out.println("Black King: moving left, right, back left, or back right?");
+				Scanner kingBlackWhereTo = new Scanner(System.in);		
+				String kingBlackMove = kingBlackWhereTo.nextLine();		// prompt user for left, right, back left, back right - STRING or CHAR??
 				
-				if(kingRedMove.equals("right")) {			// moves right			
+				if(kingBlackMove.equals("right")) {			// moves right			
 					int tempX = locX - 1;
 					int tempY = locY + 1;
-					System.out.println("I am : King, Red, moved forward right.");
+					System.out.println("King, Black, moved forward right");
 				}
-				else if(kingRedMove.equals("left")) {		// moves left
+				else if(kingBlackMove.equals("left")) {		// moves left
 					int tempX = locX + 1;
 					int tempY = locY + 1;
-					System.out.println("I am : King, Red, moved forward left.");
+					System.out.println("King, Black, moved forward left");
 				}
-				else if(kingRedMove.equals("back right")) {	// moves back right
+				else if(kingBlackMove.equals("back right")) {	// moves back right
 					int tempX = locX + 1;
 					int tempY = locY - 1;
-					System.out.println("I am : King, Red, moved backwards right.");
+					System.out.println("King, Black, moved backwards right");
 				}
-				else if(kingRedMove.equals("back left")) {	// moves back left
+				else if(kingBlackMove.equals("back left")) {	// moves back left
 					int tempX = locX - 1;
 					int tempY = locY - 1;
-					System.out.println("I am : King, Red, moved backwards right.");
+					System.out.println("King, Black, moved backwards left");
 				}
 			}
 		//---------------------------------------------------------------------
-		// black chip	
-			else if(color == true && king == false ) {				
-				System.out.println("Black: Moving left or right?");
-				Scanner blackLR = new Scanner(System.in);		
-				String blackMove = blackLR.nextLine();
-			
-					if(blackMove.equals("right")) {	// black moves right
+			//red King	
+				else if(color == false && king == true) {	
+					System.out.println("Red King: moving left, right, back left, or back right?");
+					Scanner kingRedWhereTo = new Scanner(System.in);		
+					String kingRedMove = kingRedWhereTo.nextLine();		// prompt user for left, right, back left, back right - STRING or CHAR??
+					
+					if(kingRedMove.equals("right")) {			// moves right			
+						int tempX = locX - 1;
+						int tempY = locY + 1;
+						System.out.println("King, Red, moved forward right.");
+					}
+					else if(kingRedMove.equals("left")) {		// moves left
 						int tempX = locX + 1;
 						int tempY = locY + 1;
-						System.out.println("I am Black, moved forward right.");
+						System.out.println("King, Red, moved forward left.");
 					}
-					
-					else {
-						int tempX = locX - 1;		// black moves left
-						int tempY = locY + 1;
-						System.out.println("I am Black, moved forward left.");
+					else if(kingRedMove.equals("back right")) {	// moves back right
+						int tempX = locX + 1;
+						int tempY = locY - 1;
+						System.out.println("King, Red, moved backwards right.");
 					}
-					blackLR.close();
-			}
-		
+					else if(kingRedMove.equals("back left")) {	// moves back left
+						int tempX = locX - 1;
+						int tempY = locY - 1;
+						System.out.println("King, Red, moved backwards right.");
+					}
+				}
+		//---------------------------------------------------------------------
+			// black chip	
+				else if(color == true && king == false ) {				
+					System.out.println("Black: Moving left or right?");
+					Scanner blackWhereTo = new Scanner(System.in);		
+					String blackMove = blackWhereTo.nextLine();
+				
+						if(blackMove.equals("right")) {	// black moves right
+							if (myBoard [locX +1][locY +1] = false) {
+								blackChipCounter = blackChipCounter + 1; // check if chip is red - remove icon from myboard GUI??
+								int tempX = locX + 2;
+								int tempY = locY + 2;
+								// make array space empty = null?
+								System.out.println("Black: Captured red chip and moved forward right.");
+							}
+							
+							else {
+								int tempX = locX + 1;
+								int tempY = locY + 1;
+								System.out.println("Black: moved forward right.");
+								}
+							}
+						
+						else {	// black moves left
+							if (myBoard [locX -1][locY +1] = false) {
+								blackChipCounter = blackChipCounter + 1; // check if chip is red - remove icon from myboard GUI??
+								int tempX = locX - 2;
+								int tempY = locY + 2;
+								// make array space empty = null?
+								System.out.println("Black: Captured red chip and moved forward left.");
+							}
+							
+							else {
+								int tempX = locX - 1;	
+								int tempY = locY + 1;
+								System.out.println("Black: moved forward left.");
+								}
+							}
+				}
+			
 		//---------------------------------------------------------
-		// red chip
-			else if(color == false && king == true) {				
-				System.out.println("Red: Moving left or right?");
-				Scanner redLR = new Scanner(System.in);		
-				String redMove = redLR.nextLine();
-				
-					if(redMove.equals("right")) {	// red moves right
-						int tempX = locX + 1;
-						int tempY = locY + 1;
-						System.out.println("I am Red, moved forward right.");
-					}
+			// red chip
+				else if(color == false && king == false) {				
+					System.out.println("Red: Moving left or right?");
+					Scanner redWhereTo = new Scanner(System.in);		
+					String redMove = redWhereTo.nextLine();
 					
-					else {
-						int tempX = locX - 1;		// red moves left
-						int tempY = locY + 1;
-						System.out.println("I am Red, moved forward left.");
-					}
+						if(redMove.equals("right")) {	// red moves right
+							if (myBoard [locX +1][locY +1] = true) {
+								redChipCounter = redChipCounter + 1; // check if chip is black - remove icon from myboard GUI??
+								int tempX = locX + 2;
+								int tempY = locY + 2;
+								// make array space empty = null?
+								System.out.println("Red: Captured black chip and moved forward right.");
+							}
+							
+						else {
+								int tempX = locX + 1;
+								int tempY = locY + 1;
+								System.out.println("Red: moved forward right.");
+								
+							}
+						}
+						else {	// red chip moves left
+							if (myBoard [locX -1][locY +1] == true) {
+								redChipCounter = redChipCounter + 1; // check if chip is black - remove icon from myboard GUI??
+								int tempX = locX - 2;
+								int tempY = locY + 2;
+								// make array space empty = null?
+								System.out.println("Red: Captured black chip and moved forward left.");
+							}
+							
+								else {
+									int tempX = locX - 1;		// red moves left
+									int tempY = locY + 1;
+									System.out.println("Red: moved forward left.");
+									}
+								}
+							}
+			
+		// detects for potential winner every round	
+			if (blackChipCounter == 9) {	// << Temp Condition
+				System.out.println("Black is the WINNER!!");
 			}
-	}
+				else if (redChipCounter == 9) {
+					System.out.println("Red is the WINNER!!");
+				}
+				else { // loop - next players turn
+				}
+				
+			}
 }
 
 
